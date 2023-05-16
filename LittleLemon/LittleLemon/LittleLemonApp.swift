696fc9f611896757039fcc436bd8ce75e0b4418b
Osama Mehdi <mehdi@hm.edu>
2023-05-16 14:43:43 +0200

    Add littleLemon XCode Project

---
+++ LittleLemon/LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Osama Mehdi on 16.05.23.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
